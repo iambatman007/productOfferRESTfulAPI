8c91cebeed00408c991efcf8f8a35a5993251e69
Ashish Chawla <ashishchawla90@gmail.com>
2020-03-14 09:59:21 +0000

    adding all files

---
+++ src/main/java/com/shop/productOffer/ProductOfferApplication.java
@@ -0,0 +1,13 @@
+package com.shop.productOffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOfferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductOfferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/productOffer/ProductOfferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.productOffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOfferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
